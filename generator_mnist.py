6889afa72c019f92e7ecba245aff3e0844b98220
FFTypeZero <zxiong9@illinois.edu>
2018-03-13 23:55:04 -0500

    add variable list

--- generator_mnist.py
+++ generator_mnist.py
@@ -30,6 +30,8 @@ def DeConvNormRelu(x, name, k, kernel, stride, batch_norm, reuse):
     return output
 
 def G_mnist(z, num_of_resi = 2, batch_norm = False, reuse = False):
+    start_var = set(x.name for x in tf.global_variables())
+
     output = ConvNormRelu(z, "c3s1-8", 8, [3, 3], [1, 1], batch_norm, reuse)
     output = ConvNormRelu(output, "d16", 16, [3, 3], [2, 2], batch_norm, reuse)
     output = ConvNormRelu(output, "d32", 32, [3, 3], [2, 2], batch_norm, reuse)
@@ -39,17 +41,22 @@ def G_mnist(z, num_of_resi = 2, batch_norm = False, reuse = False):
     output = DeConvNormRelu(output, "u8", 8, [3, 3], [2, 2], batch_norm, reuse)
     output = ConvNormRelu(output, "c3s1-1", 1, [3, 3], [1, 1], batch_norm, reuse)
 
-    return output
+    end_var = tf.global_variables()
+    new_var = [x for x in end_var if x.name not in start_var]
+
+    return output, new_var
 
 def test_G_mnist():
     trial_input = tf.placeholder(tf.float32, [None, 28, 28, 1])
     feed = np.zeros([25, 28, 28, 1])
-    output = G_mnist(trial_input, 4)
+    output, var_list = G_mnist(trial_input, 4)
 
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     result = sess.run(output, feed_dict={trial_input: feed})
     print(result.shape)
+    for x in var_list:
+        print(x.name)
 
 if __name__=='__main__':
     test_G_mnist()
