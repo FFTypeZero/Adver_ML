a44e552a5c8ab6ce389be5c259be8580471a905a
FFTypeZero <zxiong9@illinois.edu>
2018-05-28 16:33:39 -0500

    edit

--- all_conv.py
+++ all_conv.py
@@ -11,7 +11,7 @@ class Batch_Creator(object):
         self.data = data[0]
         self.labels = data[1]
         self.size = len(data[0])
-        
+
     def next_batch(self, batch_size):
         start = self.current_position
         end = self.current_position + batch_size
@@ -23,7 +23,7 @@ class Batch_Creator(object):
             return self.data[start: end], np.reshape(self.labels[start: end], batch_size)
 
 #Parameters Initialization
-PADDING_STRATEGY = 'VALID'
+PADDING_STRATEGY = 'SAME'
 BATCH_SIZE = 100
 TOTAL_SIZE = 50000
 NUM_STEPS_PER_EPOCH = int(TOTAL_SIZE/BATCH_SIZE)
@@ -47,22 +47,22 @@ y_ = tf.one_hot(y, depth = 10)
 in_drop = tf.contrib.layers.dropout(x, keep_prob=0.8, is_training = if_drop)
 conv1 = tf.contrib.layers.conv2d(in_drop, num_outputs=96, kernel_size=[3, 3], padding = PADDING_STRATEGY)
 conv2 = tf.contrib.layers.conv2d(conv1, num_outputs=96, kernel_size=[3, 3], padding = PADDING_STRATEGY)
-conv2_pad = tf.pad(conv2, PADDING_TENSOR)
-conv3 = tf.contrib.layers.conv2d(conv2_pad, num_outputs=96, kernel_size = [3, 3], stride = [2, 2], padding = PADDING_STRATEGY)
-conv3_pad = tf.pad(conv3, PADDING_TENSOR)
-drop1 = tf.contrib.layers.dropout(conv3_pad, keep_prob=0.5, is_training = if_drop)
+# conv2_pad = tf.pad(conv2, PADDING_TENSOR)
+conv3 = tf.contrib.layers.conv2d(conv2, num_outputs=96, kernel_size = [3, 3], stride = [2, 2], padding = PADDING_STRATEGY)
+# conv3_pad = tf.pad(conv3, PADDING_TENSOR)
+drop1 = tf.contrib.layers.dropout(conv3, keep_prob=0.5, is_training = if_drop)
 
 conv4 = tf.contrib.layers.conv2d(drop1, num_outputs=192, kernel_size=[3, 3], padding = PADDING_STRATEGY)
-conv4_pad = tf.pad(conv4, PADDING_TENSOR)
-conv5 = tf.contrib.layers.conv2d(conv4_pad, num_outputs=192, kernel_size=[3, 3], padding = PADDING_STRATEGY)
-conv5_pad = tf.pad(conv5, PADDING_TENSOR)
-conv6 = tf.contrib.layers.conv2d(conv5_pad, num_outputs=192, kernel_size=[3, 3], stride = [2, 2], padding = PADDING_STRATEGY)
-conv6_pad = tf.pad(conv6, PADDING_TENSOR)
-drop2 = tf.contrib.layers.dropout(conv6_pad, keep_prob=0.5, is_training = if_drop)
+# conv4_pad = tf.pad(conv4, PADDING_TENSOR)
+conv5 = tf.contrib.layers.conv2d(conv4, num_outputs=192, kernel_size=[3, 3], padding = PADDING_STRATEGY)
+# conv5_pad = tf.pad(conv5, PADDING_TENSOR)
+conv6 = tf.contrib.layers.conv2d(conv5, num_outputs=192, kernel_size=[3, 3], stride = [2, 2], padding = PADDING_STRATEGY)
+# conv6_pad = tf.pad(conv6, PADDING_TENSOR)
+drop2 = tf.contrib.layers.dropout(conv6, keep_prob=0.5, is_training = if_drop)
 
 conv7 = tf.contrib.layers.conv2d(drop2, num_outputs=192, kernel_size=[3, 3], padding = PADDING_STRATEGY)
-conv7_pad = tf.pad(conv7, PADDING_TENSOR)
-conv8 = tf.contrib.layers.conv2d(conv7_pad, num_outputs=192, kernel_size=[1, 1], padding = PADDING_STRATEGY)
+# conv7_pad = tf.pad(conv7, PADDING_TENSOR)
+conv8 = tf.contrib.layers.conv2d(conv7, num_outputs=192, kernel_size=[1, 1], padding = PADDING_STRATEGY)
 conv9 = tf.contrib.layers.conv2d(conv8, num_outputs=10, kernel_size=[1, 1], padding = PADDING_STRATEGY)
 
 global_ave_pool = tf.reduce_mean(tf.reduce_mean(conv9, axis=2), axis=1)
@@ -74,11 +74,12 @@ regularizer = tf.add_n([ tf.nn.l2_loss(v) for v in tvars ]) * LAMBDA
 loss = cross_entropy + regularizer
 
 #Optimizer
-values = [GAMMA*(0.1**i) for i in range(len(SCHEDULE)+1)]
-boundaries = list(NUM_STEPS_PER_EPOCH * SCHEDULE)
-global_step = tf.Variable(0, trainable = False, dtype = tf.int64)
-lr = tf.train.piecewise_constant(global_step, boundaries, values)
-train_op = tf.train.MomentumOptimizer(learning_rate = lr, momentum = MOMENTUM).minimize(loss, global_step = global_step)
+# values = [GAMMA*(0.1**i) for i in range(len(SCHEDULE)+1)]
+# boundaries = list(NUM_STEPS_PER_EPOCH * SCHEDULE)
+# global_step = tf.Variable(0, trainable = False, dtype = tf.int64)
+# lr = tf.train.piecewise_constant(global_step, boundaries, values)
+# train_op = tf.train.MomentumOptimizer(learning_rate = lr, momentum = MOMENTUM).minimize(loss, global_step = global_step)
+train_op = tf.train.AdamOptimizer(learning_rate = GAMMA).minimize(loss)
 
 correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(conv9_flat, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
